7a3a99849648cf84df2532ceda1af761e1fa2586
BurgicNikola <burgicnikola@gmail.com>
2021-08-28 15:21:14 +0200

    Adding Naming Conventions and Coding Standards project

---
+++ NamingConventionsAndCodingStandards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConventionsAndCodingStandards
+{
+    // Class names are always written in PascalCase. Example: ClientActivity.
+    // Use nouns to name classes because they represent objects.
+    class Program
+    {
+        // Method names are always written in PascalCase. Example: CalculateValue().
+        // Use verbs to name methods because they represent actions.
+        // Method arguments are always written in camelCase. Example: CalculateValue(var firstNumber, var secondNumber)
+        static void Main(string[] args)
+        {
+            // Local variables are always written in camelCase. Example: var itemCount.
+            // Avoid abbreviations. Write 'var userControl', not 'var usrCntrl'.
+            // Don't use numbers at the start of variable names.
+        }
+    }
+}
+
+// For further reference on coding standards, refer to: https://www.dofactory.com/csharp-coding-standards
