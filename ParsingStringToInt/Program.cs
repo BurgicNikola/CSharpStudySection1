c33cb358e6b088f811ae76d1ddfcf92cec787a56
BurgicNikola <burgicnikola@gmail.com>
2021-08-28 19:14:32 +0200

    Adding parsing

---
+++ ParsingStringToInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingStringToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "10";
+
+            // Converting strings into ints.
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int result = num1 + num2;
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
