66880d22418348c9c1aa15eeabad8ae3b2edcaf6
BurgicNikola <burgicnikola@gmail.com>
2021-08-28 15:51:30 +0200

    Adding Implicit and Explicit Conversion

---
+++ ImplicitAndExplicitConversion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitAndExplicitConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Implicit conversion. You can assign a value from a smaller data type to a larger one:
+            int num = 123444;
+            long bignum = num;
+
+            float myFloat = 13.353f;
+            double myNewDouble = myFloat;
+
+
+            // Cast double to int. Explicit conversion:
+            double myDouble = 13.37;
+            int myInt;
+            myInt = (int)myDouble;
+
+
+            // Type conversion. Convert double type to string type:
+            string myString = myDouble.ToString();
+
+            // Convert a float to string:
+            string mySecondString = myFloat.ToString();
+
+            // Convert a bool to string:
+            bool sunIsShining = false;
+            string myThirdString = sunIsShining.ToString();
+
+
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(mySecondString);
+            Console.WriteLine(myThirdString);
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
