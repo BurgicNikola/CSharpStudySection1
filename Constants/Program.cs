56629ec33f02cf2cab615bed4a028f66fc471726
BurgicNikola <burgicnikola@gmail.com>
2021-08-29 17:14:57 +0200

    Adding Constants

---
+++ Constants/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Constants
+{
+    // Constants are immutable values which are known at
+    // compile timeand do not change for the life of the program.
+
+    class Program
+    {
+        // Constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        const string dateOfBirth = "29.01.1991.";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"My date of birth is {dateOfBirth}");
+            Console.Read();
+        }
+    }
+}
